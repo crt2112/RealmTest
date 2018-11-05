a38f7f301cbc50d4f732ec2020656668491cddf0
beakchild <crt2112@gmail.com>
2018-11-05 17:15:29 +0900

    Initial Commit

---
+++ RealmTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RealmTest
+//
+//  Created by beakhand on 2018/11/05.
+//  Copyright © 2018年 beakhand. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
